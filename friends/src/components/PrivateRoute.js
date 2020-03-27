a0ddda94dd5e7b1f2de7a75ec3353a34b98f5752
Sam Ardis <ardis.productions@gmail.com>
2020-03-27 12:24:01 -0400

    it's workingggg, added stretch add form, delete works, edit works, MVP and Stretch met. Working on structural styling!

--- friends/src/components/PrivateRoute.js
+++ friends/src/components/PrivateRoute.js
@@ -1,22 +1,18 @@
 import React from "react"
 import { Route, Redirect } from "react-router-dom";
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
-//if user is not authorized then redirect to login
-return (
-    <Route
-      {...rest}
-      render={props => {
-        if (localStorage.getItem('token')) {
-          // user is authed
-          return <Component {...props} />;
-        } else {
-          // user not authed - redirect to /login
-          return <Redirect to="/login" />;
-        }
-      }}
-    />
-  );
-};
+const PrivateRoute = ( { component: Component, ...rest }) => {
+    const token = window.localStorage.getItem('token');
+    return (
+        <Route {...rest} render={props => {
+            if (token) {
+                return <Component {...props} />;
+            } else {
+                return <Redirect to="/login" />;
+            }
+        }}
+        />
+    )
+}
 
-export default PrivateRoute;
+export default PrivateRoute 
